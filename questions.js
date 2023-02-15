e5b03e12763ef2fd0d1649464a1d30c2850a5b7b
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-15 12:25:04 -0200

    v 3.0

---
+++ questions.js
@@ -0,0 +1,47 @@
+export default [
+    {
+      question: "Pergunta 01 : Quanto é 1 + 1 ? ",
+      answers: [
+        { option: "Quem erra na primeira é burro", correct: false },
+        { option: "2", correct: true },
+        { option: "11", correct: false },
+        { option: "3", correct: false },
+      ],
+    },
+    {
+      question: "Pergunta 02 : Quanto é 1 - 1 ?",
+      answers: [
+        { option: "Quem erra na segunda é porque deveria perder na primeira", correct: false },
+        { option: "-1", correct: false },
+        { option: "0", correct: true },
+        { option: "3", correct: false },
+      ],
+    },
+    {
+      question: "Pergunta 03 : Quanto é 1 x 1 ?",
+      answers: [
+        { option: "Até aqui você acertou na cagada", correct: false },
+        { option: "2", correct: false },
+        { option: "1", correct: true },
+        { option: "3", correct: false },
+      ],
+    },
+    {
+      question: "Pergunta 04 : Quanto é 1 / 1 ?",
+      answers: [
+        { option: "Ta ficando bom hein", correct: false },
+        { option: "0", correct: false },
+        { option: "1", correct: true },
+        { option: "3", correct: false },
+      ],
+    },
+    {
+      question: "Pergunta 05 : Quanto é 1¹ ?",
+      answers: [
+        { option: "Acho que você nem percebeu o 3 Guerreiro que removi", correct: false },
+        { option: "1", correct: true },
+        { option: "2", correct: false },
+        { option: "Removi", correct: false },
+      ],
+    },
+  ];  
