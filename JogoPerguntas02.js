4feb678e3493f4b15e78306e366ae0b410699736
danzinho007 <sedutordanzinho@hotmail.com>
2023-02-16 14:47:25 -0200

    v 3.1

--- JogoPerguntas02.js
+++ JogoPerguntas02.js
@@ -44,4 +44,44 @@ export default [
         { option: "Removi", correct: false },
       ],
     },
+    {
+      question: "Pergunta 06 : Traduza Absolutely ( Ábsâlutli )",
+      answers: [
+        { option: "O que está em () é a pronúncia", correct: false},
+        { option: "Absolutamente", correct: false },
+        { option: "Com certeza", correct: true },
+      ],
+    },
+    {
+      question: "Pergunta 07 : Traduza Deal ( Diâl )",
+      answers: [
+        { option: "Começando no inglês", correct: false },
+        { option: "Combinado", correct: true },
+        { option: "Querido", correct: false },
+      ],
+    },
+    {
+      question: "Pergunta 08 : Traduza Hurry (RãRi)",
+      answers: [
+        { option: "Essa é complicada", correct: false },
+        { option: "com pressa", correct: false},
+        { option: "Se apresse", correct: true },
+      ],
+    },
+    {
+      question: "Pergunta 09 : Traduza Sure ( Xâr )",
+      answers: [
+        { option: "Acho que é chá ", correct: false },
+        { option: "Claro", correct: true },
+        { option: "Chá", correct: false },
+      ],
+    },
+    {
+      question: "Pergunta 10 : Traduza Where ( Uéâr )",
+      answers: [
+        { option: "Eu falava Where ao invés Yeah na música da Rihanna kkkk", correct: false },
+        { option: "Enquanto", correct: false },
+        { option: "Onde", correct: true },
+      ],
+    },
   ];  
